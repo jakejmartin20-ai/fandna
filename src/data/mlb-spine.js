7014b048219e1890a777399e6b09cd533bc9e487
jakejmartin20-ai <jake.j.martin20@gmail.com>
2026-08-21 14:45:38 -0600

    Add files via upload

--- src/data/mlb-spine.js
+++ src/data/mlb-spine.js
@@ -24,14 +24,14 @@ export const scoring = Object.fromEntries(MODULE_UNIQUE.map(id => [id, mlbScorin
 export const spineScoring = {
   S6: { // build - teardown / patient / everything-now / proven
     A: { MIA:2, CWS:2, WSH:2 },
-    B: { TB:2, CLE:2, MIL:2, ATH:2, KC:2, BAL:2, MIN:2, DET:2, PIT:2, AZ:2, COL:2, CHC:2, SEA:2, TOR:2 },
-    C: { LAD:2, NYM:2, SD:2, PHI:2, HOU:2, TEX:2 },
+    B: { TB:2, CLE:2, MIL:2, ATH:2, KC:2, BAL:2, MIN:2, DET:2, PIT:3, AZ:2, COL:2, CHC:2, SEA:2, TOR:2 },
+    C: { LAD:3, NYM:2, SD:2, PHI:2, HOU:2, TEX:2 },
     D: { NYY:2, STL:2, ATL:2, SF:2, CIN:2, BOS:2, LAA:2 },
   },
   S7: { // role - favourite / underdog / glad-to-be-in / new
-    A: { NYY:2, LAD:2, HOU:2, ATL:2, STL:2 },
-    B: { TB:2, ATH:2, MIL:2, CLE:2, KC:2, SD:2, NYM:2, CWS:2 },
-    C: { CHC:2, PIT:2, SEA:2, CIN:2, DET:2, BAL:2, MIN:2, COL:2, LAA:2, SF:2, PHI:2, TOR:2, BOS:2, TEX:2 },
+    A: { NYY:2, LAD:2, HOU:2, ATL:2, STL:2, BOS:2, PHI:2 },
+    B: { TB:3, ATH:2, MIL:2, CLE:2, KC:2, SD:2, NYM:2, CWS:2 },
+    C: { CHC:2, PIT:3, SEA:2, CIN:2, DET:2, BAL:2, MIN:2, COL:2, LAA:2, SF:2, TOR:2, TEX:2 },
     D: { MIA:2, AZ:2, WSH:2 },
   },
 };
